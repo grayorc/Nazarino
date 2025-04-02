5b6fd3bd88785137720a0d847d67cd364de6aa05
grayorc <grayorc@proton.me>
2025-04-02 11:53:36 +0330

    feat(front): made header responsive and added pice section and changed sample to feature

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,62 +1,213 @@
 <!doctype html>
-<html dir="rtl" class="scroll-smooth">
+<html dir="rtl" class="scroll-smooth ">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
 </head>
 <body class="min-h-screen bg-primaryWhite">
-    <header class="flex justify-between items-center py-4 px-20 bg-PrimaryBlack sticky top-0 z-10">
-        <div class="flex justify-between gap-20" >
-            <div class="text-primaryColor text-xl">
-                <img src="dist/img/logo.png" class="w-10">
-            </div>
+    <header class="flex justify-between items-center py-4 px-5 sm:px-20 bg-PrimaryBlack sticky top-0 z-10">
+        <div class="flex justify-between items-center w-full">
+          <!-- Logo -->
+          <div class="text-primaryColor text-xl">
+            <img src="dist/img/logo.png" class="w-10" alt="Logo">
+          </div>
 
-            <ul class="flex space-x-reverse-6 gap-20">
-                <li><a href="#" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">صفحه اصلی</a></li>
-                <li><a href="#" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">قیمت‌ها</a></li>
-                <li><a href="#" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">نمونه‌ها</a></li>
-            </ul>
-        </div>
-        <div class="text-primaryWhite text-l">
+          <!-- Hamburger Menu -->
+          <div class="sm:hidden">
+            <button id="menu-toggle" class="text-primaryWhite focus:outline-none">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
+
+          <!-- Navigation Menu -->
+          <ul id="menu" class="hidden sm:flex flex-row sm:flex-row space-y-4 sm:space-y-0 sm:space-x-reverse-6 gap-5 sm:gap-20 bg-PrimaryBlack sm:bg-transparent sm:static sm:w-auto absolute top-16 right-0 w-full px-5 rounded-md shadow-lg sm:shadow-none">
+            <li><a href="#" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">صفحه اصلی</a></li>
+            <li><a href="#featurs" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">ویژگی ها</a></li>
+            <li><a href="#prices" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">قیمت‌ها</a></li>
+            <!-- Authentication Buttons -->
             @auth
-            <a href="/dashboard">
+            <li class="lg:hidden">
+              <a href="/dashboard">
                 <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
                 hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
-                transition delay-100 duration-200">داشبورد</button>
+                transition delay-100 duration-200 w-full">داشبورد</button>
+              </a>
+            </li>
+            @else
+            <li class="lg:hidden">
+              <a href="/login">
+                <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+                hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+                transition delay-100 duration-200 w-full">ورود</button>
+              </a>
+            </li>
+            @endauth
+          </ul>
+
+          <!-- Authentication Buttons for Desktop -->
+          <div class="hidden sm:block text-primaryWhite text-l">
+            @auth
+            <a href="/dashboard">
+              <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+              hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+              transition delay-100 duration-200">داشبورد</button>
             </a>
             @else
             <a href="/login">
-                <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
-                hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
-                transition delay-100 duration-200">ورود</button>
+              <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+              hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+              transition delay-100 duration-200">ورود</button>
             </a>
             @endauth
+          </div>
         </div>
-    </header>
+      </header>
 
-    <main class="flex">
-        <div id="landing" class="text-center px-6 py-8 bg-primaryBlack max-w-lvh min-h-lvh mx-auto mt-20">
-            <div class="mb-6">
-                <div class="mx-auto flex items-center justify-center animate-float">
-                    <img src="dist/img/main-logo.png" class="w-96">
+      <script>
+        // Mobile Menu Toggle
+        const menuToggle = document.getElementById('menu-toggle');
+        const menu = document.getElementById('menu');
+
+        menuToggle.addEventListener('click', () => {
+          menu.classList.toggle('hidden');
+          menu.classList.toggle('flex');
+          menu.classList.toggle('flex-col');
+          menu.classList.toggle('gap-4');
+        });
+      </script>
+
+    <main class="flex flex-col">
+        <div class="absolute top-0 -z-10 h-full w-full"><div class="absolute bottom-auto left-auto right-0 top-0 h-[300px] w-[300px] -translate-x-[30%] translate-y-[30%] rounded-full bg-primaryColor opacity-30 blur-[80px]"></div></div>
+            <div id="landing" class="text-center px-6 py-8 bg-primaryBlack max-w-lvh min-h-lvh mx-auto mt-20">
+                <div class="mb-6">
+                    <div class="mx-auto flex items-center justify-center animate-float">
+                        <img src="dist/img/main-logo.png" class="w-96">
+                    </div>
                 </div>
-            </div>
-            <h1 class="text-primaryBlack text-2xl font-bold mb-4">نظرسنجی آنلاین بسازید</h1>
-            <p class="text-secondaryWhite mb-6">راهی آسان برای دریافت بازخورد و اطلاعات مفید از مخاطبان شما.</p>
-            <div class="relative inline-flex items-center justify-center gap-4 group">
-              <div
-                class="absolute inset-0 duration-1000 opacity-60 transitiona-all bg-gradient-to-r from-primaryColor via-primaryColor to-primaryColor rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"
-              ></div>
-              <a
-                role="button"
-                class="group relative inline-flex items-center justify-center text-base rounded-xl bg-gray-900 px-8 py-3 font-semibold text-white transition-all duration-200 hover:bg-gray-800 hover:shadow-lg hover:-translate-y-0.5 hover:shadow-gray-600/30"
-                href="#"
-                >
-                شروع کنید!
-              </a>
+                <h1 class="text-primaryBlack text-2xl font-bold mb-4">نظرسنجی آنلاین بسازید</h1>
+                <p class="text-secondaryWhite mb-6">راهی آسان برای دریافت بازخورد و اطلاعات مفید از مخاطبان شما.</p>
+                <div class="relative inline-flex items-center justify-center gap-4 group">
+                    <div
+                    class="absolute inset-0 duration-1000 opacity-60 transitiona-all bg-gradient-to-r from-primaryColor via-primaryColor to-primaryColor rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"
+                    ></div>
+                    <a
+                    role="button"
+                    class="group relative inline-flex items-center justify-center text-base rounded-xl bg-gray-900 px-8 py-3 font-semibold text-white transition-all duration-200 hover:bg-gray-800 hover:shadow-lg hover:-translate-y-0.5 hover:shadow-gray-600/30"
+                    href="#"
+                    >
+                    شروع کنید!
+                </a>
+                <div class="absolute top-0 -z-10 h-full w-full"><div class="absolute bottom-auto left-autotop-0 h-[300px] w-[300px] -translate-x-[50%] translate-y-[-20%] rounded-full bg-primaryColor opacity-30 blur-[80px]"></div></div>
             </div>
         </div>
+        <div class="overflow-hidden py-24 sm:py-32 relative isolate" id="featurs">
+            <div class="absolute top-52 -z-10 h-full w-full"><div class="absolute bottom-auto left-auto right-0 top-0 h-[300px] w-[300px] -translate-x-[140%] translate-y-[30%] rounded-full bg-primaryColor opacity-30 blur-[80px]"></div></div>
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+              <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <div class="lg:pt-4 lg:pr-8">
+                  <div class="lg:max-w-lg">
+                    <h2 class="text-base/7 font-semibold text-black">مثل آب خوردن!</h2>
+                    <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-black sm:text-5xl">پلتفرم نظرسنجی نظرینو</p>
+                    <p class="mt-6 text-lg/8 text-black">ساخت نظرسنجی های عمومی و خصوصی</p>
+                    <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-black lg:max-w-none">
+                      <div class="relative pr-9">
+                        <dt class="inline font-semibold text-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 24 24" fill="currentColor"><path d="M21 3H3C2.44772 3 2 3.44772 2 4V20C2 20.5523 2.44772 21 3 21H21C21.5523 21 22 20.5523 22 20V4C22 3.44772 21.5523 3 21 3ZM12 16C10.3431 16 9 14.6569 9 13H4V5H20V13H15C15 14.6569 13.6569 16 12 16ZM16 11H13V14H11V11H8L12 6.5L16 11Z"></path></svg>
+                          ارائه خروجی روی اکسل.
+                        </dt>
+                        <dd class="inline">ارائه خروجی روی اکسل به شما این امکان را می‌دهد که داده‌ها را به‌راحتی و با فرمت قابل استفاده برای اکسل ذخیره کنید و تحلیل‌های بیشتری روی آنها انجام دهید.</dd>
+                      </div>
+                      <div class="relative pr-9">
+                        <dt class="inline font-semibold text-gray-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 24 24" fill="currentColor"><path d="M6 4H4V2H20V4H18V6C18 7.61543 17.1838 8.91468 16.1561 9.97667C15.4532 10.703 14.598 11.372 13.7309 12C14.598 12.628 15.4532 13.297 16.1561 14.0233C17.1838 15.0853 18 16.3846 18 18V20H20V22H4V20H6V18C6 16.3846 6.81616 15.0853 7.8439 14.0233C8.54682 13.297 9.40202 12.628 10.2691 12C9.40202 11.372 8.54682 10.703 7.8439 9.97667C6.81616 8.91468 6 7.61543 6 6V4ZM8 4V6C8 6.68514 8.26026 7.33499 8.77131 8H15.2287C15.7397 7.33499 16 6.68514 16 6V4H8ZM12 13.2219C10.9548 13.9602 10.008 14.663 9.2811 15.4142C9.09008 15.6116 8.92007 15.8064 8.77131 16H15.2287C15.0799 15.8064 14.9099 15.6116 14.7189 15.4142C13.992 14.663 13.0452 13.9602 12 13.2219Z"></path></svg>
+                          تعیین زمانبندی
+                        </dt>
+                        <dd class="inline">امکان تنظیم تایمر برای رأی‌گیری به شما اجازه می‌دهد بازه زمانی مشخصی برای شرکت در رأی‌گیری تعیین کنید. این ویژگی با نمایش شمارش معکوس باعث ایجاد حس فوریت و افزایش مشارکت کاربران می‌شود.</dd>                      </div>
+                      <div class="relative pr-9">
+                        <dt class="inline font-semibold text-gray-900">
+                          <svg class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path d="M2 13H8V21H2V13ZM9 3H15V21H9V3ZM16 8H22V21H16V8Z"></path>
+                          </svg>
+                          نمایش نتایج روی نمودار.
+                        </dt>
+                        <dd class="inline">امکان نمایش نتایج روی انواع نمودار به شما کمک می‌کند تا داده‌ها را به صورت بصری تحلیل کنید و مقایسه دقیق بین داده‌های مختلف انجام دهید.</dd>
+                      </div>
+                    </dl>
+                  </div>
+                </div>
+                <img src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
+              </div>
+            </div>
+          </div>
+
+        <!-- <div class="min-h-[70vh] bg-orange-600 flex flex-col" id="samples">
+            <div class="text-primaryBlack text-2xl font-bold mt-10 mb-10 mx-auto">نمونه ها</div>
+            <div class="flex mx-auto flex-shrink-1 flex-wrap gap-4">
+                <div class="bg-SecondaryWhite w-[32rem] h-72 mx-auto content-center text-center rounded-3xl">1</div>
+                <div class="bg-SecondaryWhite w-[32rem] mx-auto h-72 content-center text-center rounded-3xl">2</div>
+            </div>
+        </div> -->
+
+        <div class="relative isolate px-6 py-24 sm:py-32 lg:px-8" id="prices">
+            <div class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
+              <div class="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+            </div>
+            <div class="mx-auto max-w-4xl text-center">
+              <h2 class="text-2xl font-semibold text-primaryColor">قیمت‌ها</h2>
+            <div class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
+              <div class="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-tl-none lg:rounded-br-3xl">
+                <h3 id="tier-hobby" class="text-base/7 font-semibold text-primaryColor">عادی</h3>
+                <p class="mt-4 flex items-baseline gap-x-2">
+                  <span class="text-5xl font-semibold tracking-tight text-gray-900">۴۰٬۰۰۰ت</span>
+                  <span class="text-base text-gray-500">/ماهیانه</span>
+                </p>
+                <p class="mt-6 text-base/7 text-gray-600">پلن ماهیانه با قیمت مناسب</p>
+                <ul role="list" class="mt-8 space-y-3 text-sm/6 text-gray-600 sm:mt-10">
+                  <li class="flex gap-x-3">
+                    <svg class="h-6 w-5 flex-none text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+                    </svg>
+                    ساخت بینهایت نظرسنجی
+                  </li>
+                  <li class="flex gap-x-3">
+                    <svg class="h-6 w-5 flex-none text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+                    </svg>
+                    پشتیبانی 24 ساعته
+                  </li>
+                </ul>
+                <a href="#" aria-describedby="tier-hobby" class="mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-primaryColor ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primaryColor sm:mt-10">همین الان شروع کن!</a>
+              </div>
+              <div class="relative rounded-3xl bg-PrimaryBlack p-8 ring-1 shadow-2xl ring-gray-900/10 sm:p-10">
+                <h3 id="tier-enterprise" class="text-base/7 font-semibold text-primaryColor">حرفه ای</h3>
+                <p class="mt-4 flex items-baseline gap-x-2">
+                  <span class="text-5xl font-semibold tracking-tight text-white">۳۰۰٬۰۰۰ت</span>
+                  <span class="text-base text-gray-400">/سالیانه</span>
+                </p>
+                <p class="mt-6 text-base/7 text-gray-300">پلن سالیانه همراه با تخفیف</p>
+                <ul role="list" class="mt-8 space-y-3 text-sm/6 text-gray-300 sm:mt-10">
+                    <li class="flex gap-x-3">
+                        <svg class="h-6 w-5 flex-none text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+                        </svg>
+                        ساخت بینهایت نظرسنجی
+                        </li>
+                        <li class="flex gap-x-3">
+                        <svg class="h-6 w-5 flex-none text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+                        </svg>
+                        پشتیبانی 24 ساعته
+                    </li>
+                </ul>
+                <a href="#" aria-describedby="tier-enterprise" class="mt-8 block rounded-md bg-primaryColor px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-PrimaryBlack border-primaryColor border-2
+                hover:border-primaryColor hover:text-primaryColor
+                transition delay-100 duration-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 sm:mt-10">همین الان شروع کن!</a>
+              </div>
+            </div>
+          </div>
     </main>
 </body>
 </html>
