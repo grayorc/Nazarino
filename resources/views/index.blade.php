a4312bc6fc6feae8a7c768609cf49f4974610b19
grayorc <grayorc@proton.me>
2025-04-02 20:00:47 +0330

    feat(front) changed login changed register prepare user dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,85 +1,4 @@
-<!doctype html>
-<html dir="rtl" class="scroll-smooth ">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
-</head>
-<body class="min-h-screen bg-primaryWhite">
-    <header class="flex justify-between items-center py-4 px-5 sm:px-20 bg-PrimaryBlack sticky top-0 z-10">
-        <div class="flex justify-between items-center w-full">
-          <!-- Logo -->
-          <div class="text-primaryColor text-xl">
-            <img src="dist/img/logo.png" class="w-10" alt="Logo">
-          </div>
-
-          <!-- Hamburger Menu -->
-          <div class="sm:hidden">
-            <button id="menu-toggle" class="text-primaryWhite focus:outline-none">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
-          </div>
-
-          <!-- Navigation Menu -->
-          <ul id="menu" class="hidden sm:flex flex-row sm:flex-row space-y-4 sm:space-y-0 sm:space-x-reverse-6 gap-5 sm:gap-20 bg-PrimaryBlack sm:bg-transparent sm:static sm:w-auto absolute top-16 right-0 w-full px-5 rounded-md shadow-lg sm:shadow-none">
-            <li><a href="#" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">صفحه اصلی</a></li>
-            <li><a href="#featurs" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">ویژگی ها</a></li>
-            <li><a href="#prices" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">قیمت‌ها</a></li>
-            <!-- Authentication Buttons -->
-            @auth
-            <li class="lg:hidden">
-              <a href="/dashboard">
-                <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
-                hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
-                transition delay-100 duration-200 w-full">داشبورد</button>
-              </a>
-            </li>
-            @else
-            <li class="lg:hidden">
-              <a href="/login">
-                <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
-                hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
-                transition delay-100 duration-200 w-full">ورود</button>
-              </a>
-            </li>
-            @endauth
-          </ul>
-
-          <!-- Authentication Buttons for Desktop -->
-          <div class="hidden sm:block text-primaryWhite text-l">
-            @auth
-            <a href="/dashboard">
-              <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
-              hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
-              transition delay-100 duration-200">داشبورد</button>
-            </a>
-            @else
-            <a href="/login">
-              <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
-              hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
-              transition delay-100 duration-200">ورود</button>
-            </a>
-            @endauth
-          </div>
-        </div>
-      </header>
-
-      <script>
-        // Mobile Menu Toggle
-        const menuToggle = document.getElementById('menu-toggle');
-        const menu = document.getElementById('menu');
-
-        menuToggle.addEventListener('click', () => {
-          menu.classList.toggle('hidden');
-          menu.classList.toggle('flex');
-          menu.classList.toggle('flex-col');
-          menu.classList.toggle('gap-4');
-        });
-      </script>
-
-    <main class="flex flex-col">
+<x-inc-layout>
         <div class="absolute top-0 -z-10 h-full w-full"><div class="absolute bottom-auto left-auto right-0 top-0 h-[300px] w-[300px] -translate-x-[30%] translate-y-[30%] rounded-full bg-primaryColor opacity-30 blur-[80px]"></div></div>
             <div id="landing" class="text-center px-6 py-8 bg-primaryBlack max-w-lvh min-h-lvh mx-auto mt-20">
                 <div class="mb-6">
@@ -106,42 +25,42 @@
         <div class="overflow-hidden py-24 sm:py-32 relative isolate" id="featurs">
             <div class="absolute top-52 -z-10 h-full w-full"><div class="absolute bottom-auto left-auto right-0 top-0 h-[300px] w-[300px] -translate-x-[140%] translate-y-[30%] rounded-full bg-primaryColor opacity-30 blur-[80px]"></div></div>
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
-              <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                 <div class="lg:pt-4 lg:pr-8">
-                  <div class="lg:max-w-lg">
+                <div class="lg:max-w-lg">
                     <h2 class="text-base/7 font-semibold text-black">مثل آب خوردن!</h2>
                     <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-black sm:text-5xl">پلتفرم نظرسنجی نظرینو</p>
                     <p class="mt-6 text-lg/8 text-black">ساخت نظرسنجی های عمومی و خصوصی</p>
                     <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-black lg:max-w-none">
-                      <div class="relative pr-9">
+                    <div class="relative pr-9">
                         <dt class="inline font-semibold text-black">
                         <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 24 24" fill="currentColor"><path d="M21 3H3C2.44772 3 2 3.44772 2 4V20C2 20.5523 2.44772 21 3 21H21C21.5523 21 22 20.5523 22 20V4C22 3.44772 21.5523 3 21 3ZM12 16C10.3431 16 9 14.6569 9 13H4V5H20V13H15C15 14.6569 13.6569 16 12 16ZM16 11H13V14H11V11H8L12 6.5L16 11Z"></path></svg>
-                          ارائه خروجی روی اکسل.
+                        ارائه خروجی روی اکسل.
                         </dt>
                         <dd class="inline">ارائه خروجی روی اکسل به شما این امکان را می‌دهد که داده‌ها را به‌راحتی و با فرمت قابل استفاده برای اکسل ذخیره کنید و تحلیل‌های بیشتری روی آنها انجام دهید.</dd>
-                      </div>
-                      <div class="relative pr-9">
+                    </div>
+                    <div class="relative pr-9">
                         <dt class="inline font-semibold text-gray-900">
                             <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 24 24" fill="currentColor"><path d="M6 4H4V2H20V4H18V6C18 7.61543 17.1838 8.91468 16.1561 9.97667C15.4532 10.703 14.598 11.372 13.7309 12C14.598 12.628 15.4532 13.297 16.1561 14.0233C17.1838 15.0853 18 16.3846 18 18V20H20V22H4V20H6V18C6 16.3846 6.81616 15.0853 7.8439 14.0233C8.54682 13.297 9.40202 12.628 10.2691 12C9.40202 11.372 8.54682 10.703 7.8439 9.97667C6.81616 8.91468 6 7.61543 6 6V4ZM8 4V6C8 6.68514 8.26026 7.33499 8.77131 8H15.2287C15.7397 7.33499 16 6.68514 16 6V4H8ZM12 13.2219C10.9548 13.9602 10.008 14.663 9.2811 15.4142C9.09008 15.6116 8.92007 15.8064 8.77131 16H15.2287C15.0799 15.8064 14.9099 15.6116 14.7189 15.4142C13.992 14.663 13.0452 13.9602 12 13.2219Z"></path></svg>
-                          تعیین زمانبندی
+                        تعیین زمانبندی
                         </dt>
                         <dd class="inline">امکان تنظیم تایمر برای رأی‌گیری به شما اجازه می‌دهد بازه زمانی مشخصی برای شرکت در رأی‌گیری تعیین کنید. این ویژگی با نمایش شمارش معکوس باعث ایجاد حس فوریت و افزایش مشارکت کاربران می‌شود.</dd>                      </div>
-                      <div class="relative pr-9">
+                    <div class="relative pr-9">
                         <dt class="inline font-semibold text-gray-900">
-                          <svg class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                        <svg class="absolute top-1 right-1 size-5 text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                             <path d="M2 13H8V21H2V13ZM9 3H15V21H9V3ZM16 8H22V21H16V8Z"></path>
-                          </svg>
-                          نمایش نتایج روی نمودار.
+                        </svg>
+                        نمایش نتایج روی نمودار.
                         </dt>
                         <dd class="inline">امکان نمایش نتایج روی انواع نمودار به شما کمک می‌کند تا داده‌ها را به صورت بصری تحلیل کنید و مقایسه دقیق بین داده‌های مختلف انجام دهید.</dd>
-                      </div>
+                    </div>
                     </dl>
-                  </div>
+                </div>
                 </div>
                 <img src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
-              </div>
             </div>
-          </div>
+            </div>
+        </div>
 
         <!-- <div class="min-h-[70vh] bg-orange-600 flex flex-col" id="samples">
             <div class="text-primaryBlack text-2xl font-bold mt-10 mb-10 mx-auto">نمونه ها</div>
@@ -153,39 +72,39 @@
 
         <div class="relative isolate px-6 py-24 sm:py-32 lg:px-8" id="prices">
             <div class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
-              <div class="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+            <div class="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
             <div class="mx-auto max-w-4xl text-center">
-              <h2 class="text-2xl font-semibold text-primaryColor">قیمت‌ها</h2>
+            <h2 class="text-2xl font-semibold text-primaryColor">قیمت‌ها</h2>
             <div class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-              <div class="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-tl-none lg:rounded-br-3xl">
+            <div class="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-tl-none lg:rounded-br-3xl">
                 <h3 id="tier-hobby" class="text-base/7 font-semibold text-primaryColor">عادی</h3>
                 <p class="mt-4 flex items-baseline gap-x-2">
-                  <span class="text-5xl font-semibold tracking-tight text-gray-900">۴۰٬۰۰۰ت</span>
-                  <span class="text-base text-gray-500">/ماهیانه</span>
+                <span class="text-5xl font-semibold tracking-tight text-gray-900">۴۰٬۰۰۰ت</span>
+                <span class="text-base text-gray-500">/ماهیانه</span>
                 </p>
                 <p class="mt-6 text-base/7 text-gray-600">پلن ماهیانه با قیمت مناسب</p>
                 <ul role="list" class="mt-8 space-y-3 text-sm/6 text-gray-600 sm:mt-10">
-                  <li class="flex gap-x-3">
+                <li class="flex gap-x-3">
                     <svg class="h-6 w-5 flex-none text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
                     </svg>
                     ساخت بینهایت نظرسنجی
-                  </li>
-                  <li class="flex gap-x-3">
+                </li>
+                <li class="flex gap-x-3">
                     <svg class="h-6 w-5 flex-none text-primaryColor" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
                     </svg>
                     پشتیبانی 24 ساعته
-                  </li>
+                </li>
                 </ul>
                 <a href="#" aria-describedby="tier-hobby" class="mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-primaryColor ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primaryColor sm:mt-10">همین الان شروع کن!</a>
-              </div>
-              <div class="relative rounded-3xl bg-PrimaryBlack p-8 ring-1 shadow-2xl ring-gray-900/10 sm:p-10">
+            </div>
+            <div class="relative rounded-3xl bg-PrimaryBlack p-8 ring-1 shadow-2xl ring-gray-900/10 sm:p-10">
                 <h3 id="tier-enterprise" class="text-base/7 font-semibold text-primaryColor">حرفه ای</h3>
                 <p class="mt-4 flex items-baseline gap-x-2">
-                  <span class="text-5xl font-semibold tracking-tight text-white">۳۰۰٬۰۰۰ت</span>
-                  <span class="text-base text-gray-400">/سالیانه</span>
+                <span class="text-5xl font-semibold tracking-tight text-white">۳۰۰٬۰۰۰ت</span>
+                <span class="text-base text-gray-400">/سالیانه</span>
                 </p>
                 <p class="mt-6 text-base/7 text-gray-300">پلن سالیانه همراه با تخفیف</p>
                 <ul role="list" class="mt-8 space-y-3 text-sm/6 text-gray-300 sm:mt-10">
@@ -205,32 +124,7 @@
                 <a href="#" aria-describedby="tier-enterprise" class="mt-8 block rounded-md bg-primaryColor px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-PrimaryBlack border-primaryColor border-2
                 hover:border-primaryColor hover:text-primaryColor
                 transition delay-100 duration-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 sm:mt-10">همین الان شروع کن!</a>
-              </div>
             </div>
-          </div>
-    </main>
-
-    <footer class="bg-PrimaryBlack">
-        <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
-            <a href="#">
-                <img class="w-auto h-7" src="/dist/img/logo.png" alt="">
-            </a>
-
-            <p class="text-sm text-gray-300">© {{ verta()->year }}</p>
-
-            <div class="flex -mx-2">
-                <a href="https://github.com/grayorc" class="mx-2  transition-colors duration-300 text-gray-300 hover:text-primaryColor" aria-label="Github">
-                    <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z">
-                        </path>
-                    </svg>
-                </a>
             </div>
         </div>
-    </footer>
-
-
-</body>
-</html>
+</x-inc-layout>
---
+++ resources/views/layouts/inc.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl" class="scroll-smooth">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="min-h-screen bg-primaryWhite">
+
+        <header class="flex justify-between items-center py-4 px-5 sm:px-20 bg-PrimaryBlack sticky top-0 z-10">
+            <div class="flex justify-between items-center w-full">
+            <!-- Logo -->
+            <div class="text-primaryColor text-xl">
+                <img src="dist/img/logo.png" class="w-10" alt="Logo">
+            </div>
+
+            <!-- Hamburger Menu -->
+            <div class="sm:hidden">
+                <button id="menu-toggle" class="text-primaryWhite focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                </button>
+            </div>
+
+            <!-- Navigation Menu -->
+            <ul id="menu" class="hidden sm:flex flex-row sm:flex-row space-y-4 sm:space-y-0 sm:space-x-reverse-6 gap-5 sm:gap-20 bg-PrimaryBlack sm:bg-transparent sm:static sm:w-auto absolute top-16 right-0 w-full px-5 rounded-md shadow-lg sm:shadow-none">
+                <li><a href="#" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">صفحه اصلی</a></li>
+                <li><a href="#featurs" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">ویژگی ها</a></li>
+                <li><a href="#prices" class="text-primaryWhite hover:text-primaryColor transition delay-100 duration-200">قیمت‌ها</a></li>
+                <!-- Authentication Buttons -->
+                @auth
+                <li class="lg:hidden">
+                <a href="/dashboard">
+                    <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+                    hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+                    transition delay-100 duration-200 w-full">داشبورد</button>
+                </a>
+                </li>
+                @else
+                <li class="lg:hidden">
+                <a href="/login">
+                    <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+                    hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+                    transition delay-100 duration-200 w-full">ورود</button>
+                </a>
+                </li>
+                @endauth
+            </ul>
+
+            <!-- Authentication Buttons for Desktop -->
+            <div class="hidden sm:block text-primaryWhite text-l">
+                @auth
+                <a href="/dashboard">
+                <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+                hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+                transition delay-100 duration-200">داشبورد</button>
+                </a>
+                @else
+                <a href="/login">
+                <button class="bg-primaryColor rounded-md px-8 py-0.5 border-primaryColor border-2
+                hover:bg-PrimaryBlack hover:border-primaryColor hover:text-primaryColor
+                transition delay-100 duration-200">ورود</button>
+                </a>
+                @endauth
+            </div>
+            </div>
+        </header>
+        <script>
+            // Mobile Menu Toggle
+            const menuToggle = document.getElementById('menu-toggle');
+            const menu = document.getElementById('menu');
+
+            menuToggle.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+            menu.classList.toggle('flex');
+            menu.classList.toggle('flex-col');
+            menu.classList.toggle('gap-4');
+            });
+        </script>
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        <footer class="bg-PrimaryBlack">
+            <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
+                <a href="#">
+                    <img class="w-auto h-7" src="/dist/img/logo.png" alt="">
+                </a>
+
+                <p class="text-sm text-gray-300">© {{ verta()->year }}</p>
+
+                <div class="flex -mx-2">
+                    <a href="https://github.com/grayorc" class="mx-2  transition-colors duration-300 text-gray-300 hover:text-primaryColor" aria-label="Github">
+                        <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z">
+                            </path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
